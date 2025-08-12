7743780227dad66738d919fba970ea2c4b07506a
tranp6648 <tranp6648@gmail.com>
2025-08-12 18:47:09 +0700

    update

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes,Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
+import RoomPage from './pages/RoomPage'
 function App() {
 
   return (
   <Routes>
     <Route element={<Layout/>}>
     <Route path='/' element={<HomePage/>}/>
+    <Route path='/Room' element={<RoomPage/>}/>
     </Route>
   </Routes>
   )
---
+++ fe/src/components/Room/BannerRoom.tsx
@@ -0,0 +1,12 @@
+const BannerRoom=()=>{
+    return(
+        <div className="w-full h-[1000px] overflow-hidden">
+            <img
+            src="https://cache.marriott.com/is/image/marriotts7prod/si-sgnsi-studio-twin-27424:Feature-Hor?wid=1920&fit=constrain"
+            alt="Banner"
+            className="w-full h-full object-cover"
+            />
+        </div>
+    )
+}
+export default BannerRoom;
