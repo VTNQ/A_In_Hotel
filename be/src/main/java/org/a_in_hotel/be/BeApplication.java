1163aa8ee7a4a9f49198ba7ac4fda25cbec75a48
PhongTranAffina <phong.tran@affina.com.vn>
2025-09-24 00:48:01 +0700

    change mono

---
+++ be/src/main/java/org/a_in_hotel/be/BeApplication.java
@@ -0,0 +1,13 @@
+package org.a_in_hotel.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeApplication.class, args);
+	}
+
+}
