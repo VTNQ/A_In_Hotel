b5a0925e4c50abed710474a8a5c3d79da196fb60
PhongTranAffina <phong.tran@affina.com.vn>
2025-09-23 09:41:26 +0700

    add security banner

--- be/banner-service/src/main/java/com/example/bannerservice/config/OpenApiConfig.java
+++ be/banner-service/src/main/java/com/example/bannerservice/config/OpenApiConfig.java
@@ -22,7 +22,7 @@ public class OpenApiConfig {
                         .description("API cho hệ thống Banner")
                         .contact(new Contact().name("WebVibe").email("tranp6648@gmail.com"))
                 )
-                .addServersItem(new Server().url("http://localhost:8282/service/banner"))
+                .addServersItem(new Server().url("https://banner.ainhotelvn.com/service/banner"))
                 .components(new Components()
                         .addSecuritySchemes(scheme,
                                 new SecurityScheme()
