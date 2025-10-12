249cecf130726d1e2171954491325b13c3872578
dangvanphucnghia <nghia812014@gmail.com>
2025-10-12 16:45:03 +0700

    update fe user by nghia

--- fe/User/src/pages/HomePage.tsx
+++ fe/User/src/pages/HomePage.tsx
@@ -3,12 +3,13 @@ import HeroSlider from "../components/HeroSection";
 import AboutSection from "../components/Home/AboutSection";
 import RatingSection from "../components/Home/RatingSection";
 import FacilitiesSection from "../components/Home/FacilitiesSection";
-
+import RoomShowcase from "../components/Home/RoomShowcase";
 
 const HomePage = () => {
  return(
   <>
   <HeroSlider/>
+  <RoomShowcase /> 
   <AboutSection/>
   <RatingSection/>
   <FacilitiesSection/>  
